a5f14e2e6d2b7a1129170859b2f0d4a40953961a
Farhan Haziq <kl2311015157@student.uptm.edu.my>
2025-11-26 20:58:34 +0800

    update v2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <--- 1. IMPORT THIS
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensures Vercel knows where the build output is
-  },
-});
+  plugins: [
+    react(),
+    tailwindcss(), // <--- 2. ADD THIS
+  ],
+})
