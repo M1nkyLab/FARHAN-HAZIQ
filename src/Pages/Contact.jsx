edee9c3b652f333cee44b7196f4742620f41c285
Farhan Haziq <kl2311015157@student.uptm.edu.my>
2025-12-07 20:26:44 +0800

    Add new resume for the website

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -61,7 +61,7 @@ function Contact() {
         </a>
 
         <a 
-          href="https://drive.google.com/file/d/1933zm3_78kCu3VT9VWhF1Ibv7xriFm82/view?usp=sharing" 
+          href="https://drive.google.com/file/d/1tztWLKc-R-AuMOKRnGhoYYoTVawb-g2C/view?usp=sharing" 
           target="_blank" 
           rel="noopener noreferrer"
           className={`w-full text-2xl lg:text-4xl text-center py-4 hover:text-gray-300 transition-colors ${animatedUnderlineClass}`}
